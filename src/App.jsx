e8f30e1f15e414525e4f09d2c4d7ceb02a05ce2f
reecem2022 <reecem2022@fau.edu>
2025-01-29 11:36:06 -0500

    Added Construction Info

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ const App=()=>{
       <div className="header-container">
         <img src={pfp} className="profile-picture" />
         <p style={{fontSize:"45px",fontWeight:"bold"}}>Mark Reece</p>
+        <p>CURRENTLY UNDER CONSTRUCTION</p>
         <p>Software Engineer/Developer and Game Programmer</p>
         <p>Majoring in Computer Science, Minoring in Artifical Intelligence and Cyber Security</p>
       </div>
